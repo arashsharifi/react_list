ebabfc5a032b6216f2cecb5ba52253bc9459dfe7
arash amirsharifi <arashamirsharifi1970@gmail.com>
2023-10-30 14:37:03 +0330

    add loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import Items from "./components/items";
+import Loader from "./components/loader";
 import TabssContainer from "./components/tabsContainer";
 import Header from "./layout/header";
 import Wrapper from "./layout/wrapper";
 
 const App = () => {
+  const [Loading, setLoding] = useState(true);
+
+  setTimeout(() => {
+    setLoding(false);
+  }, 3000);
+
   return (
     <Wrapper>
-      <Header>
-        <TabssContainer />
-      </Header>
-      <Items />
+      {Loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header>
+            <TabssContainer />
+          </Header>
+          <Items />
+        </>
+      )}
     </Wrapper>
   );
 };
--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,8 +1,15 @@
 //بزن rafce exprtion function
 //rfce     decleration function
-
+import Spinner from "react-bootstrap/Spinner";
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <>
+      <h1 className="m-auto mb-3">React Lists App</h1>
+      <Spinner animation="border" role="status" className="m-auto">
+        <span className="visually-hidden">Loading</span>
+      </Spinner>
+    </>
+  );
 };
 
 export default Loader;
